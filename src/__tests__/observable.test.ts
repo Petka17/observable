fc9d7586fa54bf78ee86d061ec8e1b096703aeda
Petr Klimenko <petrklim@yandex.ru>
2019-03-25 23:05:05 +0300

    add of and filter with tests

--- src/__tests__/observable.test.ts
+++ src/__tests__/observable.test.ts
@@ -1,5 +1,38 @@
 import Observable from "../index";
 
+test("of with not array", () => {
+  let complete = false;
+
+  Observable.of("123").subscribe({
+    next(v) {
+      expect(v).toEqual("123");
+    },
+    complete() {
+      complete = true;
+    }
+  });
+
+  expect(complete).toBe(true);
+});
+
+test("of with array", () => {
+  let complete = false;
+  const arr = [1, 2, 3];
+  let res = [];
+
+  Observable.of(arr).subscribe({
+    next(v) {
+      res.push(v);
+    },
+    complete() {
+      complete = true;
+    }
+  });
+
+  expect(res).toEqual(arr);
+  expect(complete).toBe(true);
+});
+
 test("timeout", done => {
   let k = false;
 
@@ -12,4 +45,46 @@ test("timeout", done => {
       done();
     }
   });
+});
+
+test("map", () => {
+  let complete = false;
+  const arr = [1, 2, 3];
+  const projectionFn = (x: number) => x * 2;
+  let res = [];
+
+  Observable.of(arr)
+    .map(projectionFn)
+    .subscribe({
+      next(v) {
+        res.push(v);
+      },
+      complete() {
+        complete = true;
+      }
+    });
+
+  expect(res).toEqual(arr.map(projectionFn));
+  expect(complete).toBe(true);
+});
+
+test("filter", () => {
+  let complete = false;
+  const arr = [1, 2, 3];
+  const predicateFn = (x: number) => x % 2 === 1;
+  let res = [];
+
+  Observable.of(arr)
+    .filter(predicateFn)
+    .subscribe({
+      next(v) {
+        res.push(v);
+      },
+      complete() {
+        complete = true;
+      }
+    });
+
+  expect(res).toEqual(arr.filter(predicateFn));
+  expect(complete).toBe(true);
 });
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ interface Subscription {
 }
 
 type SubscribeFn = (observer: Observer) => Subscription;
+type PredicateFn = (v: any) => boolean;
 
 class Observable {
   private _subscribe: SubscribeFn;
@@ -17,8 +18,24 @@ class Observable {
     this._subscribe = subscribe;
   }
 
-  subscribe(observer: Observer) {
+  subscribe(observer: Observer): Subscription {
     return this._subscribe(observer);
+  }
+
+  static of(value: any) {
+    return new Observable(function subscribe(observer: Observer) {
+      if (value instanceof Array) {
+        value.forEach(v => observer.next(v));
+      } else {
+        observer.next(value);
+      }
+
+      observer.complete();
+
+      return {
+        unsubscribe() {}
+      };
+    });
   }
 
   static timeout(time: number) {
@@ -60,6 +77,30 @@ class Observable {
         next(v: any) {
           try {
             observer.next(projection(v));
+          } catch (err) {
+            observer.error(err);
+          }
+        },
+        error(err: Error) {
+          observer.error(err);
+        },
+        complete() {
+          observer.complete();
+        }
+      });
+    });
+  }
+
+  public filter(predicate: PredicateFn) {
+    const self = this;
+
+    return new Observable(function subscribe(observer: Observer) {
+      return self.subscribe({
+        next(v: any) {
+          try {
+            if (predicate(v)) {
+              observer.next(v);
+            }
           } catch (err) {
             observer.error(err);
           }
